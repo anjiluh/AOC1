3453cce9e88f0d5ea0c68763c61da5986e03beac
Angela Waters <angro6963@gmail.com>
2013-04-10 15:06:53 -0400

    initial commit

---
+++ AOC1Tests/AOC1Tests.h
@@ -0,0 +1,13 @@
+//
+//  AOC1Tests.h
+//  AOC1Tests
+//
+//  Created by Angela Ross on 4/9/13.
+//  Copyright (c) 2013 Angela Waters. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface AOC1Tests : SenTestCase
+
+@end
